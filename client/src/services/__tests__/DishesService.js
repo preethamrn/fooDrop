66890803246ef9426228ee7f1831d913c851b99d
unknown <karthik_ramesh@outlook.com>
2018-11-15 12:30:18 -0800

    unit testing

---
+++ client/src/services/__tests__/DishesService.js
@@ -0,0 +1,36 @@
+import DishesService from "../DishesService"
+import MockDishesService from "MockDishesService"
+import Api from "../Api"
+
+it("fetches data from axios", async() => {
+  MockDishesService.searchDish.mockImplementation( (params) => 
+   	Promise.resolve( Api().get('dish/get_dishes_by_radius', {params})))
+
+  var x = await MockDishesService.searchDish({
+  		  name: "ssadf",
+          ingredients: ["[asdfff, fff]"],
+          dietaryRestrictions: ["qqqqqqqqqqqqq", "ee"],
+          lat: 34.0653079,
+          lon: -118.45305689999999,
+          radius: 100,
+          priceLow: 0,
+          priceHigh: 100
+  	});
+  // UNCOMMENT VAR Y CODE  BELOW AND ADJUST EXPECT STATEMENT REQS APPROPRIATELY 
+  // var y = await MockDishesService.searchDish({
+  // 		  name: "watup",
+  //         ingredients: ["[asdfff, fff]"],
+  //         dietaryRestrictions: ["qqqqqqqqqqqqq", "ee"],
+  //         lat: 34.0653079,
+  //         lon: -118.45305689999999,
+  //         radius: 100,
+  //         priceLow: 0,
+  //         priceHigh: 100
+  // 	});
+
+  expect(MockDishesService.searchDish).toHaveBeenCalledTimes(1);
+  expect(MockDishesService.searchDish).toHaveBeenCalledWith(    // only for the first one
+  	 {"dietaryRestrictions": ["qqqqqqqqqqqqq", "ee"], "ingredients": ["[asdfff, fff]"], "lat": 34.0653079, "lon": -118.45305689999999, "name": "ssadf", "priceHigh": 100, "priceLow": 0, "radius": 100}
+  ); 
+ 
+});
