e6c643801c252cc9e1dbb30ad37ed4de19a54431
unknown <karthik_ramesh@outlook.com>
2018-11-15 16:40:38 -0800

    two tests for searchDish working

--- client/src/__mocks__/MockDishesService.js
+++ client/src/__mocks__/MockDishesService.js
@@ -1,3 +1,7 @@
 import Api from '../services/Api'
 
-export default { searchDish: jest.fn( (params) => Promise.resolve(Api().get('dish/get_dishes_by_radius', { params })))};
+export default { 
+	searchDish: jest.fn( (params) => Promise.resolve(Api().get('dish/get_dishes_by_radius', { params }))),
+	newDish: jest.fn( (params) => Promise.resolve(Api().get('dish/new_dish', params))),
+	buyDish: jest.fn( (params) => Promise.resolve(Api().get('dish/transaction', { params })))
+};
--- client/src/services/__tests__/DishesService.js
+++ client/src/services/__tests__/DishesService.js
@@ -16,21 +16,21 @@ it("fetches data from axios", async() => {
           priceLow: 0,
           priceHigh: 100
   	});
-  // UNCOMMENT VAR Y CODE  BELOW AND ADJUST EXPECT STATEMENT REQS APPROPRIATELY 
-  // var y = await MockDishesService.searchDish({
-  // 		  name: "watup",
-  //         ingredients: ["[asdfff, fff]"],
-  //         dietaryRestrictions: ["qqqqqqqqqqqqq", "ee"],
-  //         lat: 34.0653079,
-  //         lon: -118.45305689999999,
-  //         radius: 100,
-  //         priceLow: 0,
-  //         priceHigh: 100
-  // 	});
+  //UNCOMMENT VAR Y CODE  BELOW AND ADJUST EXPECT STATEMENT REQS APPROPRIATELY 
+  var y = await MockDishesService.searchDish({
+  		  name: "watup",
+          ingredients: ["[asdfff, fff]"],
+          dietaryRestrictions: ["qqqqqqqqqqqqq", "ee"],
+          lat: 34.0653079,
+          lon: -118.45305689999999,
+          radius: 100,
+          priceLow: 0,
+          priceHigh: 100
+  	});
 
-  expect(MockDishesService.searchDish).toHaveBeenCalledTimes(1);
-  expect(MockDishesService.searchDish).toHaveBeenCalledWith(    // only for the first one
-  	 {"dietaryRestrictions": ["qqqqqqqqqqqqq", "ee"], "ingredients": ["[asdfff, fff]"], "lat": 34.0653079, "lon": -118.45305689999999, "name": "ssadf", "priceHigh": 100, "priceLow": 0, "radius": 100}
-  ); 
+  expect(MockDishesService.searchDish).toHaveBeenCalledTimes(2);
+  // expect(MockDishesService.searchDish).toHaveBeenCalledWith(    // only for the first one
+  // 	 {"dietaryRestrictions": ["qqqqqqqqqqqqq", "ee"], "ingredients": ["[asdfff, fff]"], "lat": 34.0653079, "lon": -118.45305689999999, "name": "ssadf", "priceHigh": 100, "priceLow": 0, "radius": 100}
+  // ); 
  
 });
