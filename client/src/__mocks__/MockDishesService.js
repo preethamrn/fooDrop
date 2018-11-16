315aa1c54235a49af799c29e6b4241682781f1c9
unknown <karthik_ramesh@outlook.com>
2018-11-15 16:30:18 -0800

    fixed small bug for 1st test: get_dishes_by_radius

--- client/src/__mocks__/MockDishesService.js
+++ client/src/__mocks__/MockDishesService.js
@@ -1,16 +1,3 @@
 import Api from '../services/Api'
 
-export default {
-	searchDish: jest.fn((params) => Promise.resolve(Api().get('dish/get_dishes_by_radius', { params })
-)};
-
-// 	console.log(Promise.resolve(Api().get('dish/get_dishes_by_radius', 
-// 		{
-//   			priceLow: 3,
-//   			priceHigh: 100, 
-//   			lat: 34.0653079, 
-//   			lon: -118.45305689999999, 
-//   			radius: 100, 
-//   			ingredients: ["peanut", "chicken"]
-//   	}))
-// );
+export default { searchDish: jest.fn( (params) => Promise.resolve(Api().get('dish/get_dishes_by_radius', { params })))};
