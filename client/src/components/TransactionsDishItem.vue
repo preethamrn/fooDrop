9992d355200f21c1a417b1cdb93658d2b4718a10
preethamrn <preetham.in.1997@gmail.com>
2018-11-29 14:00:23 -0800

    Updated listings dish item UI to be more usable

--- client/src/components/TransactionsDishItem.vue
+++ client/src/components/TransactionsDishItem.vue
@@ -12,7 +12,11 @@
             <v-flex xs9 sm5>
               <v-list-tile-content>
                 <v-list-tile-title>{{ name }}</v-list-tile-title>
-                <v-list-tile-sub-title>Location: {{ location }}</v-list-tile-sub-title>
+                <v-list-tile-sub-title>
+                  <a target='_blank' :href="'https://www.google.com/maps/search/' + location.lat + ',' + location.lon">
+                    Distance: {{ distanceInMiles }} miles (Google Maps 🡕)
+                  </a>
+                </v-list-tile-sub-title>
               </v-list-tile-content>
             </v-flex>
             <v-flex xs6 sm3>
@@ -48,7 +52,31 @@ export default {
     DishDetails
   },
   data () {
-    return { }
+    return {
+      distanceInMiles: 0.0
+    }
+  },
+  mounted () {
+    if (navigator.geolocation) {
+       var self = this;
+       navigator.geolocation.getCurrentPosition(function (position) {
+        var lat1 = position.coords.latitude
+        var lon1 = position.coords.longitude
+        var lat2 = self.location.lat
+        var lon2 = self.location.lon
+        var R = 6371; // Radius of the earth in km
+        var dLat = (lat2-lat1) * (Math.PI/180);
+        var dLon = (lon2-lon1) * (Math.PI/180);
+        var a = 
+          Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(lat1 * (Math.PI/180)) * Math.cos(lat2 * (Math.PI/180)) * 
+          Math.sin(dLon/2) * Math.sin(dLon/2)
+          ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        var d = R * c * 1.6; // Distance in Miles
+        self.distanceInMiles = Math.round(d*100)/100;
+      })
+    }
   },
   props: {
     name: String,
