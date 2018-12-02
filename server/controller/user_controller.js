609f964b1589dad1f3470691534a7ef42deb39cd
Leslie Liang <lliang9838@gmail.com>
2018-12-02 15:39:07 -0800

    added documentation for user_controller

--- server/controller/user_controller.js
+++ server/controller/user_controller.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose');
 var User = require("../models/user");
 const ObjectId = mongoose.Types.ObjectId;
 
+/** 
+ * User Controller Module
+ * @module User_Controller
+ */
 
+/**
+     * Updates a user based on user_id with values in the values array
+     * @param {String} user_id - user_id of the user we are trying to update
+     * @param {String[]} values - array of values we are trying to update to
+     * @param {callback} callback - callback function that is called after processing is done
+     */
 exports.updateUser = function(user_id,values,callback){
 
     User.findById(user_id, function(error, user){
